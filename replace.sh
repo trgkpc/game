199fad9d67dc1b00493f167314951b6c13aedfda
trgkpc <trgkpc@gmail.com>
2019-08-11 21:22:46 +0900

    replace.shのバグを修正。

--- replace.sh
+++ replace.sh
@@ -2,7 +2,7 @@ dir_names=("card" "player" "test")
 for dir_name in ${dir_names}
     do
     cd ${dir_name}
-    file_names=`ls ${dir_name}`
+    file_names=`ls`
     for file_name in ${file_names}
         do
         sed s/player/driver/g ${file_name} > tmp.cpp
