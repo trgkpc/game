dbc9d8beaaa924b87b3ef754e8c4a5056992e267
trgkpc <trgkpc@gmail.com>
2019-08-11 23:53:09 +0900

    testのプログラムをまとめてmakeする関数を作った

---
+++ test/make.sh
@@ -0,0 +1,10 @@
+echo "compile test programs..."
+test_names=("accident" "cards"  "enum_int_conv" "repair" "right_of_way" "roll" "run" "stop")
+for test_name in ${test_names[@]}
+    do
+    echo "compiling " ${test_name} " ....."
+    out_name="test_"${test_name}".out"
+    file_name=${test_name}".cpp"
+    g++ -o ${out_name} ${file_name}
+done
+echo "Fin."
