89f6fb30a49ee47b84a15becdd5f23f904c17476
trgkpc <trgkpc@gmail.com>
2019-08-12 13:19:50 +0900

    card.hppに変更を反映

--- card/card.hpp
+++ card/card.hpp
@@ -11,101 +11,101 @@
 namespace Card
 {
 /***** Distance Card ****/
-DistanceCard make_25mile_card()
+std::shared_ptr<DistanceCard> make_25mile_card()
 {
-    return DistanceCard(distance_25, 25);
+    return std::make_shared<DistanceCard>(distance_25, 25);
 }
 
-DistanceCard make_50mile_card()
+std::shared_ptr<DistanceCard> make_50mile_card()
 {
-    return DistanceCard(distance_50, 50);
+    return std::make_shared<DistanceCard>(distance_50, 50);
 }
 
-DistanceCard make_75mile_card()
+std::shared_ptr<DistanceCard> make_75mile_card()
 {
-    return DistanceCard(distance_75, 75);
+    return std::make_shared<DistanceCard>(distance_75, 75);
 }
 
-DistanceCard make_100mile_card()
+std::shared_ptr<DistanceCard> make_100mile_card()
 {
-    return DistanceCard(distance_100, 100);
+    return std::make_shared<DistanceCard>(distance_100, 100);
 }
 
-DistanceCard make_200mile_card()
+std::shared_ptr<DistanceCard> make_200mile_card()
 {
-    return DistanceCard(distance_200, 200);
+    return std::make_shared<DistanceCard>(distance_200, 200);
 }
 
 /***** Hazard Card ****/
-HazardCard make_out_of_gas_card()
+std::shared_ptr<HazardCard> make_out_of_gas_card()
 {
-    return HazardCard(out_of_gas, Driver::DriverStatus::OutOfGas, Driver::SafetiesKind::ExtraTank);
+    return std::make_shared<HazardCard>(out_of_gas, Driver::DriverStatus::OutOfGas, Driver::SafetiesKind::ExtraTank);
 }
 
-HazardCard make_flat_tire_card()
+std::shared_ptr<HazardCard> make_flat_tire_card()
 {
-    return HazardCard(flat_tire, Driver::DriverStatus::FlatTire, Driver::SafetiesKind::PunctureProof);
+    return std::make_shared<HazardCard>(flat_tire, Driver::DriverStatus::FlatTire, Driver::SafetiesKind::PunctureProof);
 }
 
-HazardCard make_accident_card()
+std::shared_ptr<HazardCard> make_accident_card()
 {
-    return HazardCard(accident, Driver::DriverStatus::Accident, Driver::SafetiesKind::DrivingAce);
+    return std::make_shared<HazardCard>(accident, Driver::DriverStatus::Accident, Driver::SafetiesKind::DrivingAce);
 }
 
-SpeedLimit make_speed_limit_card()
+std::shared_ptr<SpeedLimit> make_speed_limit_card()
 {
-    return SpeedLimit();
+    return std::make_shared<SpeedLimit>();
 }
 
-HazardCard make_stop_card()
+std::shared_ptr<HazardCard> make_stop_card()
 {
-    return HazardCard(stop, Driver::DriverStatus::Stop, Driver::SafetiesKind::RightOfWay);
+    return std::make_shared<HazardCard>(stop, Driver::DriverStatus::Stop, Driver::SafetiesKind::RightOfWay);
 }
 
 /***** Remedies Card ****/
-RemediesCard make_gasoline_card()
+std::shared_ptr<RemediesCard> make_gasoline_card()
 {
-    return RemediesCard(gasoline, Driver::DriverStatus::OutOfGas);
+    return std::make_shared<RemediesCard>(gasoline, Driver::DriverStatus::OutOfGas);
 }
 
-RemediesCard make_spare_tire_card()
+std::shared_ptr<RemediesCard> make_spare_tire_card()
 {
-    return RemediesCard(spare_tire, Driver::DriverStatus::FlatTire);
+    return std::make_shared<RemediesCard>(spare_tire, Driver::DriverStatus::FlatTire);
 }
 
-RemediesCard make_repair_card()
+std::shared_ptr<RemediesCard> make_repair_card()
 {
-    return RemediesCard(repair, Driver::DriverStatus::Accident);
+    return std::make_shared<RemediesCard>(repair, Driver::DriverStatus::Accident);
 }
 
-EndOfLimit make_end_of_limit_card()
+std::shared_ptr<EndOfLimit> make_end_of_limit_card()
 {
-    return EndOfLimit();
+    return std::make_shared<EndOfLimit>();
 }
 
-Roll make_roll_card()
+std::shared_ptr<Roll> make_roll_card()
 {
-    return Roll();
+    return std::make_shared<Roll>();
 }
 
 /***** Safeties Card ****/
-SafetiesCard make_extra_tank_card()
+std::shared_ptr<SafetiesCard> make_extra_tank_card()
 {
-    return SafetiesCard(extra_tank, Driver::SafetiesKind::ExtraTank);
+    return std::make_shared<SafetiesCard>(extra_tank, Driver::SafetiesKind::ExtraTank);
 }
 
-SafetiesCard make_puncture_proof_card()
+std::shared_ptr<SafetiesCard> make_puncture_proof_card()
 {
-    return SafetiesCard(puncture_proof, Driver::SafetiesKind::PunctureProof);
+    return std::make_shared<SafetiesCard>(puncture_proof, Driver::SafetiesKind::PunctureProof);
 }
 
-SafetiesCard make_driving_ace_card()
+std::shared_ptr<SafetiesCard> make_driving_ace_card()
 {
-    return SafetiesCard(driving_ace, Driver::SafetiesKind::DrivingAce);
+    return std::make_shared<SafetiesCard>(driving_ace, Driver::SafetiesKind::DrivingAce);
 }
 
-SafetiesCard make_right_of_way_card()
+std::shared_ptr<SafetiesCard> make_right_of_way_card()
 {
-    return SafetiesCard(right_of_way, Driver::SafetiesKind::RightOfWay);
+    return std::make_shared<SafetiesCard>(right_of_way, Driver::SafetiesKind::RightOfWay);
 }
 }  // namespace Card
