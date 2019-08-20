61176975888ca856a39b49fc273dc43a311fde48
trgkpc <trgkpc@gmail.com>
2019-08-20 16:45:52 +0900

    testのコンパイル時にresultを出力するようにした

--- test/make.sh
+++ test/make.sh
@@ -4,7 +4,9 @@ for test_name in ${test_names[@]}
     do
     echo "compiling " ${test_name} " ....."
     out_name="test_"${test_name}".out"
+    result_file_name="result_"${test_name}".log"
     file_name=${test_name}".cpp"
     g++ -o ${out_name} ${file_name}
+    ./${out_name} > ${result_file_name}
 done
 echo "Fin."
