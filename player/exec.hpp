09dd1c9aa04321990bca633a2ddd53f446871e75
trgkpc <trgkpc@gmail.com>
2019-08-21 09:06:13 +0900

    vectorを用いてcardを管理することにした

--- player/exec.hpp
+++ player/exec.hpp
@@ -1,19 +1,36 @@
 #include "../card/card.hpp"
+#include <algorithm>
+#include <random>
+#include <vector>
 namespace Exec
 {
-template <int n>
 struct Exec {
     Exec()
     {
     }
-    void init(std::array<double, 19> cards)
+    void card_init(std::array<double, 19> cards)
     {
+        deck = {};
         for (int i = 0; i < 19; i++) {
             card_array.at(i) = Card::make_card(i);
+            for (int j = 0; j < cards.at(i); j++) {
+                deck.emplace_back(i);
+            }
         }
+        shuffle();
+    }
+    void shuffle()
+    {
+        std::random_device get_rand;
+        std::mt19937 get_rand_mt(get_rand());
+        std::shuffle(vec.begin(), vec.end(), get_rand_mt);
     }
 
 private:
     std::array<std::shared_ptr<AbstCard>, 19> card_array;
+    std::vector<int> deck;
+    std::vector<int> drivers;
+    std::vector<int> players;
+    std::vector<std::vector<int>> players_on_drivers;
 };
 }  //namespace Exec
--- player/player.hpp
+++ player/player.hpp
@@ -1,5 +1,7 @@
 #include "../card/card.hpp"
 #include "../driver/driver.hpp"
+#include <algorithm>
+#include <vector>
 
 namespace Player
 {
@@ -8,11 +10,19 @@ struct Player {
     {
     }
 
-    void use_card(std::shared_ptr<Card::AbstCard> card, Driver::Driver& target)
+    bool use_card(Card::Kind kind, std::shared_ptr<Card::AbstCard> card, Driver::Driver& target)
     {
-        card->main(driver, target);
+        auto itr = std::find(cards.begin(), cards.end(), kind);
+        if (itr == cards.end()) {
+            return false;
+        } else {
+            card->main(driver, target);
+            cards.erase(itr);
+            return true;
+        }
     }
     Driver::Driver& driver;
     int id;
+    std::vector<Card::Kind> cards;
 };
 }  //namespace Player
